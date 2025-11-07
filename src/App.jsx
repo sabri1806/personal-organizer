95e9efee4824e512b8f25f1fde80ede1ba87a53e
Sabrina Lucero <sabrina.lucero@MacCha13.local>
2025-11-07 17:08:42 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='max-w-xl mx-auto p-4'>
+     <h1 className='text-2xl font-bold mb-4 pt-16'>Personal organizer</h1>
+     <div>Task form input</div>
+     <div>Task list</div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/redux/features/taskSlice.js
@@ -0,0 +1,58 @@
+import { createSlice } from '@reduxjs/toolkit'
+import { act } from 'react'
+
+const loadDataFromLocalStorage = () => {
+    try {
+        const data = localStorage.getItem('trying to save tasks')
+        return data ? JSON.parse(data) : []
+    } catch (error) {
+        return []
+    }
+}
+
+const saveDataToLocalStorage = (tasks) => {
+    localStorage.setItem("trying to save tasks", JSON.stringify(tasks))
+}
+
+const taskSlice = createSlice({
+    name: 'tasks',
+    initialState: {
+        tasks: [],
+        filter: {
+            status:'all',
+            search: '',
+
+        }
+    },
+    reducers:{
+        //for each feature
+        addTask: (state, action) => {
+          state.tasks.push(action.payload);
+          saveDataToLocalStorage(state.tasks)
+        },
+        deleteTask: (state, action) => {
+            state.tasks = state.tasks.filter(task => task.id !== action.payload);
+            saveDataToLocalStorage(state.tasks)
+        },
+        toogleTasksCompleted: (state,action) => {
+            const task = state.tasks.find(t => t.id === action.payload);
+            if (task) task.completed = !task.completed;
+            saveDataToLocalStorage(state.tasks)
+        },
+        editTask: (state, action) => {
+            const {id, nextText } = action.payload;
+            const task = state.tasks.find(t => t.id === id);
+            if (task) task.text = nextText;
+            saveDataToLocalStorage(state.tasks)
+        },
+        setStatusFilter: (state, action) => {
+            state.filter.status = action.payload;
+        },
+        setSearchFilter: (state, action) => {
+            state.filter.search = action.payload;
+        }
+    }
+})
+
+export const { addTask, deleteTask, toogleTasksCompleted, editTask, setStatusFilter, setSearchFilter } = taskSlice.actions;
+export default taskSlice.reducer;
